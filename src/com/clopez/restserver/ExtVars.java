4008bb2bcaeac8a906babcd57645b539ec879a53
Carlos Lopez <cluis.lopez@gmail.com>
2019-12-23 16:59:28 +0100

    Json properties implemented

---
+++ src/com/clopez/restserver/ExtVars.java
@@ -0,0 +1,32 @@
+package com.clopez.restserver;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ExtVars {
+	public int Port;
+	public String Root;
+	public String AppPath;
+	public List<Map<String, String>> Servlets;
+	
+	public ExtVars() {
+		Servlets = new ArrayList<>();
+		Port = 0;
+		Root="";
+		AppPath="";
+	}
+	
+	@Override
+	public String toString() {
+		String ret = "Port: " + Port +"\n"
+		+ "Root Web Server: " + Root +"\n"
+		+ "Servlets Java Path: " + AppPath +"\n"
+		+ "Mount Points" +"(" + Servlets.size() +")\n";
+		for (Map<String, String> m: Servlets)
+			ret = ret + m.get("MountPoint") + " : " + m.get("ClassName") + "\n";
+		
+		return ret;
+	}
+}
--- src/com/clopez/restserver/Server.java
+++ src/com/clopez/restserver/Server.java
@@ -3,17 +3,23 @@ package com.clopez.restserver;
 import java.io.BufferedInputStream;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.reflect.Type;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 import java.util.logging.FileHandler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 public class Server {
 
@@ -41,21 +47,25 @@ public class Server {
 		fd.setFormatter(formatter);
 
 		// Inicializando fichero de propiedades
-		InputStream in = null;
-		try {
-			in = new FileInputStream("Properties");
-			BufferedInputStream fd = new BufferedInputStream(in);
-			props.load(fd);
-		} catch (FileNotFoundException e) {
-			log.log(Level.SEVERE, "Error de entrada/salida al abrir el fichero de Propiedades");
-			log.log(Level.SEVERE, e.toString(), e);
-		}
-
+		Gson json = new Gson();
+		FileReader reader = new FileReader("Properties.json");
+		Type listType = new TypeToken<ExtVars>(){}.getType();
+		ExtVars props = json.fromJson(reader, listType);
+		
 		// Mapeando puntos de montaje //
 		mountPoints = new HashMap<>();
-		mount("/Tester", "com.clopez.restserver.Tester");
+		if (props.Servlets.size() ==0) {
+			log.log(Level.INFO, "No mounted servlets");
+		} else {
+			for (Map<String, String> m: props.Servlets) {
+				mount(m.get("MountPoint"), props.AppPath + "." + m.get("ClassName"));
+				log.log(Level.INFO, "Mounting URI " + m.get("MountPoint") + " into " + props.AppPath + "." + m.get("ClassName"));
+			}
+		}
+			
+		//mount("/Tester", "com.clopez.restserver.Tester");
 
-		final ServerSocket server = new ServerSocket(Integer.parseInt(props.getProperty("Port")));
+		final ServerSocket server = new ServerSocket(props.Port);
 		System.out.println("Arrancando el servidor");
 		log.log(Level.INFO, "Server started");
 
